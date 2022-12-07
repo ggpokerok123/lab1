836179652058306fb06daad13c47f22394015735
vlano003 <85347360+vlano003@users.noreply.github.com>
2022-12-08 00:38:42 +0200

    lab1full

---
+++ lab1/task1.rb
@@ -0,0 +1,8 @@
+puts "Enter x:"
+x=gets.chomp.to_f
+puts "Enter a:"
+a=gets.chomp.to_f
+puts "Enter t:"
+t=gets.chomp.to_f
+
+puts F= (1/Math::tan((Math::log10(1.61803)+16.3).abs/Math::sqrt( Math::E**2 - Math::sqrt(a**3)).abs))+(6.8+Math::cos(Math::PI-t**2))/Math::sin(a)**3;
